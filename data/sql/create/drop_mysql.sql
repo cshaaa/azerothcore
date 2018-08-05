8f20c1630ffcdb0deeabaa236b9c7efb009fa2bc
drsi <drsi@debian>
2018-08-05 11:23:34 +0800

    汉化

--- data/sql/create/drop_mysql.sql
+++ data/sql/create/drop_mysql.sql
@@ -1,12 +1,12 @@
 REVOKE ALL PRIVILEGES ON * . * FROM 'acore'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `world` . * FROM 'acore'@'localhost';
+REVOKE ALL PRIVILEGES ON `world1` . * FROM 'acore'@'localhost';
 
-REVOKE GRANT OPTION ON `world` . * FROM 'acore'@'localhost';
+REVOKE GRANT OPTION ON `world1` . * FROM 'acore'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `characters` . * FROM 'acore'@'localhost';
+REVOKE ALL PRIVILEGES ON `characters1` . * FROM 'acore'@'localhost';
 
-REVOKE GRANT OPTION ON `characters` . * FROM 'acore'@'localhost';
+REVOKE GRANT OPTION ON `characters1` . * FROM 'acore'@'localhost';
 
 REVOKE ALL PRIVILEGES ON `auth` . * FROM 'acore'@'localhost';
 
@@ -14,8 +14,8 @@ REVOKE GRANT OPTION ON `auth` . * FROM 'acore'@'localhost';
 
 DROP USER 'acore'@'localhost';
 
-DROP DATABASE IF EXISTS `world`;
+DROP DATABASE IF EXISTS `world1`;
 
-DROP DATABASE IF EXISTS `characters`;
+DROP DATABASE IF EXISTS `characters1`;
 
 DROP DATABASE IF EXISTS `auth`;
